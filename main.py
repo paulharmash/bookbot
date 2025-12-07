6e5d1702d293f15a01f95aaa1cf4de7752d4a9cc
paulharmash <pavlo.harmash@gmail.com>
2025-12-07 14:38:33 +0100

    CH3L2: adding terminal argument

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import sys
 from stats import count_words
 from stats import number_of_characters
 from stats import sorted_list
 from stats import remove_nonalph
-path = "books/frankenstein.txt"
 
+# Function to get text input
+def text_input():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        return sys.argv[1]
+
+# Function to convert a text in a string
 def get_book_text(path):
     with open(path) as f:
         return f.read() # Returns the text as a string
 
-    
+#Function to print results in the console    
 def print_report(path, num_words, sorted_chars):
-    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {path}...\n----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for item in sorted_chars:
         print(f"{item['char']}: {item['num']}")
 
 def main():
+    path = text_input()
     text = get_book_text(path)
     num_words = count_words(text)
     char_dict = number_of_characters(text) 
