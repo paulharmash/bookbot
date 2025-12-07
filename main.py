ba2e4c0addfc8d413f3b027e0182cba7fdf747fe
paulharmash <pavlo.harmash@gmail.com>
2025-12-07 14:02:12 +0100

    CH3L1: adding remove_nonalph() and sorted_lists() functions

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from stats import count_words
 from stats import number_of_characters
+from stats import sorted_list
+from stats import remove_nonalph
 path = "books/frankenstein.txt"
 
 def get_book_text(path):
     with open(path) as f:
         return f.read() # Returns the text as a string
 
+    
+def print_report(path, num_words, sorted_chars):
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+
 def main():
     text = get_book_text(path)
     num_words = count_words(text)
-    dictionary_of_symbols = number_of_characters(text) 
-    print(f"Found {num_words} total words")
-    print(dictionary_of_symbols)
+    char_dict = number_of_characters(text) 
+    alpha_char = remove_nonalph(char_dict)
+    sorted_chars = sorted_list(alpha_char)
+    print_report(path, num_words, sorted_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
-def count_words(text): # A general function that would work with any text
-    return len(text.split()) # Returns the number of words
+# Function to calculate words in a text
+def count_words(text): 
+    return len(text.split())
 
+# Function to calcualte the number of characters in a text
 def number_of_characters(text):
     dictionary = {} # Dictionary for the result of number_of_characters()
     lowercase_text = text.lower() # Making the text all lowercase
@@ -9,3 +11,29 @@ def number_of_characters(text):
             dictionary[character] = 0 # Adding keys if they don't exist
         dictionary[character] += 1 # Adding values
     return dictionary
+
+# Function to remove non-alphabetical symbols    
+def remove_nonalph(char_dict):
+    alpha_dict = {}
+    for symbol in char_dict:
+        if symbol.isalpha():
+           alpha_dict[symbol] = char_dict[symbol]
+        else:
+            continue
+    
+    return alpha_dict
+
+# Function to sort the results of number_of_characters(). Here we convers a dictionary in a list of dictionaries
+def sorted_list(dictionary):
+    char_dict = [] # List of dictionaries
+    for pair in dictionary:
+        sorted_list_item = {} # Dictionary item to combine in the result list
+        sorted_list_item["char"] = pair
+        sorted_list_item["num"] = dictionary[pair]
+        char_dict.append(sorted_list_item)
+    
+    def sort_on(items):
+        return items["num"]
+    char_dict.sort(reverse=True, key=sort_on)
+        
+    return char_dict
